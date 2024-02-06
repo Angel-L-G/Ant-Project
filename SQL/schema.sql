394ded419411119a08c297c41a46e2dbead0e9af
Angel-L-G <angelchiquitopons@gmail.com>
2024-02-06 16:24:28 +0000

    feat: first aproach of the db complited #1

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS `ants`;
 DROP TABLE IF EXISTS `nests`;
 
 CREATE TABLE `usuarios` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
   `password` varchar(200) NOT NULL,
   `rol` varchar(45) NOT NULL,
@@ -25,17 +25,6 @@ CREATE TABLE `friends` (
   CONSTRAINT pk_friends PRIMARY KEY(id),
   CONSTRAINT fk_user FOREIGN KEY (id_user) REFERENCES usuarios(id),
   CONSTRAINT fk_friend FOREIGN KEY (id_friend) REFERENCES usuarios(id)
-)
-
-CREATE TABLE `ants` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(40),
-  `type` VARCHAR(30),
-  `life` INT,
-  `damage` INT,
-  `working` INT,
-  `cost` INT,
-  CONSTRAINT pk_ants PRIMARY KEY(id)
 );
 
 CREATE TABLE `nests` (
@@ -44,4 +33,43 @@ CREATE TABLE `nests` (
   `ant_type` VARCHAR(40),
   CONSTRAINT pk_ants PRIMARY KEY(id),
   CONSTRAINT fk_user FOREIGN KEY (id_user) REFERENCES usuarios(id)
+);
+
+CREATE TABLE `usuario_nest` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_user` INT,
+  `id_nest` INT,
+  CONSTRAINT pk_ants PRIMARY KEY(id),
+  CONSTRAINT fk_user FOREIGN KEY (id_user) REFERENCES usuarios(id),
+  CONSTRAINT fk_nest FOREIGN KEY (id_nest) REFERENCES ants(id)
+);
+
+CREATE TABLE `ants` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(40),
+  `type` VARCHAR(30),
+  `life` INT,
+  `damage` INT,
+  `working` boolean,
+  `cost` INT,
+  CONSTRAINT pk_ants PRIMARY KEY(id)
+);
+
+CREATE TABLE `ant_nest` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_nest` INT NOT NULL,
+  `id_ant` INT NOT NULL,
+  `cuantity` INT NOT NULL,
+  CONSTRAINT pk_ant_nest PRIMARY KEY(id),
+  CONSTRAINT fk_nest FOREIGN KEY (id_nest) REFERENCES ants(id)
+  CONSTRAINT fk_ant FOREIGN KEY (id_ant) REFERENCES nests(id)
+);
+
+CREATE TABLE `bosses` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(40),
+  `life` INT,
+  `damage` INT,
+  `reward` INT,
+  CONSTRAINT pk_boss PRIMARY KEY(id),
 );
