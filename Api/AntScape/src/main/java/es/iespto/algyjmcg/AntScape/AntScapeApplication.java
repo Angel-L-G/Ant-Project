c21d294fc324bec7b733e1524123728f697ea00f
Angel-L-G <angelchiquitopons@gmail.com>
2024-02-10 16:55:04 +0000

    feat: project init #2

---
+++ Api/AntScape/src/main/java/es/iespto/algyjmcg/AntScape/AntScapeApplication.java
@@ -0,0 +1,13 @@
+package es.iespto.algyjmcg.AntScape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntScapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntScapeApplication.class, args);
+	}
+
+}
---
+++ Api/AntScape/src/test/java/es/iespto/algyjmcg/AntScape/AntScapeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iespto.algyjmcg.AntScape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntScapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
