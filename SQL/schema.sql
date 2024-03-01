6fe8f2db6bd684455049466933bc65b1a6d64d9d
Angel-L-G <angelchiquitopons@gmail.com>
2024-03-01 19:53:13 +0000

    refactor: refactor of the database for the new logic of the game #1

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -5,14 +5,23 @@ USE AntScape;
 DROP TABLE IF EXISTS `usuarios`;
 DROP TABLE IF EXISTS `ants`;
 DROP TABLE IF EXISTS `nests`;
-
-DROP DATABASE IF EXISTS antscape;
-CREATE DATABASE antscape;
-USE antscape;
-
-DROP TABLE IF EXISTS `usuarios`;
+DROP TABLE IF EXISTS `guild`;
+DROP TABLE IF EXISTS `guild_levels`;
+DROP TABLE IF EXISTS `friends`;
+DROP TABLE IF EXISTS `nests_levels`;
 DROP TABLE IF EXISTS `ants`;
-DROP TABLE IF EXISTS `nests`;
+DROP TABLE IF EXISTS `ants_user`;
+DROP TABLE IF EXISTS `bosses`;
+
+CREATE TABLE `guild` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` varchar(45),
+  `trophys` INT NOT NULL DEFAULT(1),
+  `quantity` INT DEFAULT(1),
+  `defense_range` INT DEFAULT(1-6),
+  `defense_number` INT DEFAULT(null),
+  constraint pk_guild PRIMARY KEY(id)
+);
 
 CREATE TABLE `usuarios` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -23,21 +32,13 @@ CREATE TABLE `usuarios` (
   `active` BOOLEAN DEFAULT false,
   `hash` varchar(255),
   `banned` BOOLEAN DEFAULT false,
-  `eggs` varchar DEFAULT ("10"),
-  `golden_eggs` varchar DEFAULT ("0"),
-  `img` varchar DEFAULT("profile.png"),
+  `eggs` varchar(10) DEFAULT("10"),
+  `golden_eggs` varchar(10) DEFAULT ("0"),
+  `img` varchar(255) DEFAULT("profile.png"),
   `id_guild` INT DEFAULT(null),
   constraint pk_usuarios PRIMARY KEY(id),
-  CONSTRAINT fk_guild FOREIGN KEY (id_guild) REFERENCES guild(id),
+  CONSTRAINT fk_guild_user FOREIGN KEY (id_guild) REFERENCES guild(id),
   constraint uk_name UNIQUE KEY(name)
-);
-
-CREATE TABLE `guild` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` varchar(45),
-  `quantity` INT DEFAULT(1),
-  `defense_range` INT DEFAULT(1-6),
-  `defense_number` INT DEFAULT(null),
 );
 
 CREATE TABLE `guild_levels` (
@@ -46,10 +47,10 @@ CREATE TABLE `guild_levels` (
   `name` varchar(30) NOT NULL,
   `cost` INT NOT NULL,
   `level` INT NOT NULL,
-  `efect` varchar,
+  `efect` varchar(255),
   CONSTRAINT pk_guild_levels PRIMARY KEY(id),
   CONSTRAINT fk_guild FOREIGN KEY (id_guild) REFERENCES guild(id),
-  CONSTRAINT uk_nest_level UNIQUE(nest_id, level)
+  CONSTRAINT uk_nest_level UNIQUE(id_guild, level)
 );
 
 CREATE TABLE `friends` (
@@ -61,9 +62,28 @@ CREATE TABLE `friends` (
   CONSTRAINT fk_friend FOREIGN KEY (id_friend) REFERENCES usuarios(id)
 );
 
+CREATE TABLE `ants` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(40),
+  `type` VARCHAR(30),
+  `biome` VARCHAR(20),
+  `description` VARCHAR(100),
+  CONSTRAINT pk_ants PRIMARY KEY(id),
+  constraint uk_name UNIQUE KEY(name, type)
+);
+
+CREATE TABLE `ant_user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_user` INT,
+  `id_ant` INT NOT NULL,
+  CONSTRAINT pk_ants PRIMARY KEY(id),
+  CONSTRAINT fk_userant FOREIGN KEY (id_user) REFERENCES usuarios(id),
+  CONSTRAINT fk_antuser FOREIGN KEY (id_ant) REFERENCES ants(id)
+);
+
 CREATE TABLE `nests` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `id_ant` VARCHAR(40),
+  `id_ant` INT NOT NULL,
   `id_user` INT NOT NULL,
   `deleted` BOOLEAN DEFAULT false,
   CONSTRAINT pk_ants PRIMARY KEY(id),
@@ -75,30 +95,13 @@ CREATE TABLE `nest_levels` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nest_id` INT NOT NULL,
   `name` varchar(30),
+  `production` INT NOT NULL,
   `cost` INT NOT NULL,
   `level` INT NOT NULL,
   `multiplier` DECIMAL(10, 2) NOT NULL,
   CONSTRAINT pk_nest_levels PRIMARY KEY(id),
   CONSTRAINT fk_nest_level FOREIGN KEY (nest_id) REFERENCES nests(id),
   CONSTRAINT uk_nest_level UNIQUE(nest_id, level)
-);
-
-CREATE TABLE `ants` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(40),
-  `type` VARCHAR(30),
-  `biome` VARCHAR(20),
-  CONSTRAINT pk_ants PRIMARY KEY(id),
-  constraint uk_name UNIQUE KEY(name, type)
-);
-
-CREATE TABLE `ant_user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_user` INT,
-  `id_ant` INT NOT NULL,
-  CONSTRAINT pk_ants PRIMARY KEY(id),
-  CONSTRAINT fk_userant FOREIGN KEY (id_user) REFERENCES usuarios(id),
-  CONSTRAINT fk_antuser FOREIGN KEY (id_ant) REFERENCES nests(id)
 );
 
 CREATE TABLE `bosses` (
