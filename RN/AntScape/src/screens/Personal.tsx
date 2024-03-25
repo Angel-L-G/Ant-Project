edbfe264924f33ddbe3d1c81c29e900db24973e9
JavierCedres <javmarcego@gmail.com>
2024-03-25 14:41:42 +0000

    feat(React-Native): Ahora por mucho dinero que tengas no se sale de su lugar ya que, por ejemplo 100000 es 100.00K. #25

--- RN/AntScape/src/screens/Personal.tsx
+++ RN/AntScape/src/screens/Personal.tsx
@@ -37,6 +37,28 @@ const Personal = ({navigation}: Props) => {
         getOwnNests();
     }, [])
 
+    function abreviarNumero(valor: number): string {
+        if (valor < 10000) {
+            return valor.toString();
+        } else if (valor < 1000000) {
+            const parteDecimal = Math.floor((valor % 1000) / 10); // Extraer la parte decimal y redondear hacia abajo
+            const parteEntera = Math.floor(valor / 1000);
+            return `${parteEntera}.${parteDecimal.toString().padStart(2, '0')}K`;
+        } else if (valor < 1000000000) {
+            const parteDecimal = Math.floor((valor % 1000000) / 10000); // Extraer la parte decimal y redondear hacia abajo
+            const parteEntera = Math.floor(valor / 1000000);
+            return `${parteEntera}.${parteDecimal.toString().padStart(2, '0')}M`;
+        } else if (valor < 1000000000000) {
+            const parteDecimal = Math.floor((valor % 1000000000) / 10000000); // Extraer la parte decimal y redondear hacia abajo
+            const parteEntera = Math.floor(valor / 1000000000);
+            return `${parteEntera}.${parteDecimal.toString().padStart(2, '0')}B`;
+        } else {
+            const parteDecimal = Math.floor((valor % 1000000000000) / 10000000000); // Extraer la parte decimal y redondear hacia abajo
+            const parteEntera = Math.floor(valor / 1000000000000);
+            return `${parteEntera}.${parteDecimal.toString().padStart(2, '0')}T`;
+        }
+    }
+
     async function nuevaRama() {
         console.log(lastLevel);
 
@@ -144,13 +166,13 @@ const Personal = ({navigation}: Props) => {
                     <TouchableOpacity onPress={() => goToProfile()} style={{width: "10%", height: "80%"}}>
                         <Image source={require('../assets/imgs/profile.png')} style={{width: 50, height: "100%", borderRadius: 100}} />
                     </TouchableOpacity>
-                    <Image source={require('../assets/imgs/tablon.png')} style={{ width: "16%", height: "60%", borderRadius: 100 }} />
-                    <View style={{ position: 'absolute', marginLeft: 101, justifyContent: 'center', alignItems: 'center', backgroundColor: "rgba(255, 255, 255, 0.4)", width: "16%", height: "60%", borderRadius: 100, flexDirection: 'row' }}>
-                        <Text style={{ color: "black", fontWeight: "bold" }}>{eg.current}</Text>
+                    <Image source={require('../assets/imgs/tablon.png')} style={{ width: "20%", height: "60%", borderRadius: 100 }} />
+                    <View style={{ position: 'absolute', marginLeft: 92, justifyContent: 'center', alignItems: 'center', backgroundColor: "rgba(255, 255, 255, 0.4)", width: "20%", height: "60%", borderRadius: 100, flexDirection: 'row' }}>
+                        <Text style={{ color: "black", fontWeight: "bold" }}>{abreviarNumero(eg.current)}</Text>
                         <Image source={require('../assets/imgs/FireAntEgg.webp')} style={{ width: "18%", height: "60%", borderRadius: 100, marginLeft: 5 }} />
                     </View>
-                    <Image source={require('../assets/imgs/tablon.png')} style={{ width: "16%", height: "60%", borderRadius: 100 }} />
-                    <View style={{ position: 'absolute', marginLeft: 226, justifyContent: 'center', alignItems: 'center', backgroundColor: "rgba(255, 255, 255, 0.4)", width: "16%", height: "60%", borderRadius: 100, flexDirection: 'row' }}>
+                    <Image source={require('../assets/imgs/tablon.png')} style={{ width: "20%", height: "60%", borderRadius: 100 }} />
+                    <View style={{ position: 'absolute', marginLeft: 226, justifyContent: 'center', alignItems: 'center', backgroundColor: "rgba(255, 255, 255, 0.4)", width: "20%", height: "60%", borderRadius: 100, flexDirection: 'row' }}>
                         <Text style={{ color: "black", fontWeight: "bold" }}>{user.goldenEggs}</Text>
                         <Image source={require('../assets/imgs/GoldenAntEgg2.png')} style={{ width: "18%", height: "60%", borderRadius: 100, marginLeft: 5 }} />
                     </View>
